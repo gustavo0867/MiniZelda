50df0a751e09543e70d4e8b0d5501e9bae3e4b95
[200~gustavo0867 <gbrasilreis08@gmail.com>
2024-03-25 20:41:33 -0400

    testando

--- zeldaminiclone/src/zeldaminiclone/Blocks.java
+++ zeldaminiclone/src/zeldaminiclone/Blocks.java
@@ -12,7 +12,7 @@ public class Blocks extends Rectangle {
 	
 	
 	public void render(Graphics g) {
-		g.setColor(Color.GREEN);
+		g.setColor(Color.RED);
 		g.fillRect(x,y,width,height);
 		g.setColor(Color.black);
 		g.drawRect(x, y, width, height);
